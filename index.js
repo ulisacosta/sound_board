9d9723190cff93ce2719399e3b0059ec4a7482c7
ulises <ulisacost@gmail.com>
2023-10-10 19:30:43 -0300

    add sounds

---
+++ index.js
@@ -0,0 +1,45 @@
+const trumpetsButton = document.getElementById("trumpetsButton");
+const monkeyButton = document.getElementById("monkeyButton");
+const laughButton = document.getElementById("laughButton");
+const victoryButton = document.getElementById("victoryButton");
+const drumsButton = document.getElementById("drumsButton");
+
+trumpetsButton.addEventListener("click", () => {
+  trumpetsMp3.play();
+  monkeyMp3.pause();
+laughMp3.pause();
+victoryMp3.pause();
+drumsMp3.pause();
+});
+
+monkeyButton.addEventListener("click", () => {
+  monkeyMp3.play();
+  trumpetsMp3.pause();
+  laughMp3.pause();
+  victoryMp3.pause();
+  drumsMp3.pause();
+});
+
+laughButton.addEventListener("click", () => {
+  laughMp3.play();
+  trumpetsMp3.pause();
+  monkeyMp3.pause();
+  victoryMp3.pause();
+  drumsMp3.pause();
+});
+
+victoryButton.addEventListener("click", () => {
+  victoryMp3.play();
+  trumpetsMp3.pause();
+  monkeyMp3.pause();
+  laughMp3.pause();
+  drumsMp3.pause();
+});
+
+drumsButton.addEventListener("click", () => {
+  drumsMp3.play();
+  trumpetsMp3.pause();
+  laughMp3.pause();
+  victoryMp3.pause();
+  monkeyMp3.pause();
+});
